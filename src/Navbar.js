87ebe13be2f9a230181c874922557823b94c243f
Pree46 <preethirajagopal464@gmail.com>
2023-12-22 14:42:26 +0530

    Repository Crated for Nipix Tech

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+function Navbar() {
+    return( 
+    <div>
+        <nav id="navbar">
+        <div class="content">
+            <p>NIPIX TECH</p>
+        </div>
+        <ul class="nav-sections">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Learn</a></li>
+            <li><a href="/">Contact Us</a></li>
+            <li><a href="/">Sign Up</a></li>
+        </ul>
+        </nav>
+        
+    </div>
+    )
+}
+export default Navbar;
