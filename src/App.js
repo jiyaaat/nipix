636cd2d954bcfaea948461acdb4d1e768b626e6c
Jiya Thakur <142887456+jiyaaat@users.noreply.github.com>
2024-01-19 19:40:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Learn from './Learn';
 import Navbar from './Navbar';
 import Home from './Home';
 import ContactUs from './Contact';
 import SignUp from './SignUp';
-import LearningPath from './LearningPath';
-import './Navbar.css';
+import LearningPath from './LearningPath'; 
+import Quiz from './Quiz';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Learn" element={<Learn />} />
-                    <Route path="/ContactUs" element={<ContactUs />} />
-                    <Route path="/SignUp" element={<SignUp />} />
-                    <Route path='/Learn/Physics' element={<LearningPath />}/>
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Learn" element={<Learn />} />
+          <Route path="/Learn/LearningPath" element={<LearningPath />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/Learn/LearningPath/Quiz"element={<Quiz/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
